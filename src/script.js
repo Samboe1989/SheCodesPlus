f835d8823f7fe94bdf640e638639d3b8430773f3
Samboe1989 <91028218+Samboe1989@users.noreply.github.com>
2022-01-12 16:32:50 +0100

    Added API cal for forecast

--- src/script.js
+++ src/script.js
@@ -48,7 +48,7 @@ dateTimeElement.innerHTML = formatDate(now);
 
 //display forecast
 
-function displayForecast() {
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -73,6 +73,13 @@ function displayForecast() {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
+
+function getForecast(coordinates) {
+  let apiKey = "6d9d93b7d32e34850e611e89547fc660";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 //Search function
 
 function showWeather(response) {
@@ -98,6 +105,8 @@ function showWeather(response) {
   );
 
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coords);
 }
 
 function searchCity(event) {
@@ -167,5 +176,3 @@ function changeImg() {
     actualweatherImg.setAttribute("src", "images/Rainy2.jpg");
   }
 }
-
-displayForecast();
